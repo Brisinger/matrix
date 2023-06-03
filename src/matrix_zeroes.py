61dc26b705c7353989a2b15f39748eeb256f66b8
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2023-06-03 14:07:42 +0530

    Depth-First search constant space complexity

--- src/matrix_zeroes.py
+++ src/matrix_zeroes.py
@@ -75,6 +75,57 @@ def count_zeroes(matrix: list[list[int]]) -> list[int]:
     return result
 
 
+def zeroes(matrix: list[list[int]]) -> list[int]:
+    """
+    Counts the no. of contiguous zeroes by traversing horizontally, vertically and diagonally.
+    It doesn't use additional memory for storing results of depth-first search.
+    Depth-First search uses two parameters returning the count of zeroes during unwinding phase.
+
+    Args:
+    -----
+        matrix (list:list[int]): Input square matrix.
+
+    Returns:
+    --------
+        list[int]: List containing no. of contiguous zeroes in square matrix.
+    """
+    _n = len(matrix)
+    _directions = ((0, 1), (1, 0), (-1, 0), (0, -1),
+                   (-1, 1), (-1, -1), (1, -1), (1, 1))
+    result = []
+
+    def dfs(row: int, col: int) -> int:
+        """
+        Performs depth-first search from a given row and column of square matrix.
+        Move left, right, up , down and diagonally untill adjacent 0's are found.
+        Change the value of cell in given row and column of matrix if 0 so that its not visited again.
+
+        Args:
+        -----
+            row (int): Row index of input matrix.
+            col (int): Column index of input matrix.
+
+        Returns:
+        --------
+           int: No. of contiguous 0's from given row and column index of matrix
+        """
+        count = 0
+        matrix[row][col] = -1
+        for point in _directions:
+            x, y = (point[0] + row), (point[1] + col)
+            if 0 <= x < _n and 0 <= y < _n and matrix[x][y] == 0:
+                count += dfs(x, y)
+        return count + 1
+    # Traverse the matrix to detect contiguous zeroes from each cell.
+    for row in range(_n):
+        for col in range(_n):
+            count = 0
+            if matrix[row][col] == 0:
+                count = dfs(row, col)
+                result.append(count)
+    return result
+
+
 if __name__ == "__main__":
     _input = [[0, 2, 3, 0],
               [0, 1, 0, 2],
@@ -90,4 +141,11 @@ if __name__ == "__main__":
               [0, 2, 1, 0]
               ]
     ans = count_zeroes(_input)
+    print(ans)
+    _input = [[0, 2, 3, 0],
+              [0, 1, 0, 0],
+              [2, 1, 0, 2],
+              [0, 2, 1, 0]
+              ]
+    ans = zeroes(_input)
     print(ans)
--- tests/test_matrix_zeroes.py
+++ tests/test_matrix_zeroes.py
@@ -3,7 +3,7 @@ Testing no. of contiguous zeroes in matrix.
 """
 import unittest
 from unittest import TestCase
-from src.matrix_zeroes import zeroes_count, count_zeroes
+from src.matrix_zeroes import zeroes_count, count_zeroes, zeroes
 
 
 class TestMatrixZeroes(TestCase):
@@ -25,8 +25,14 @@ class TestMatrixZeroes(TestCase):
                        [2, 0, 0, 2],
                        [0, 2, 1, 0]
                        ]
+        self.input3 = [[0, 2, 3, 0],
+                       [0, 1, 0, 0],
+                       [2, 1, 0, 2],
+                       [0, 2, 1, 0]
+                       ]
         self.expected1 = [2, 3, 1]
         self.expected2 = [8]
+        self.expected3 = [2, 5, 1]
         return super().setUp()
 
     def test_matrix_zeroes(self) -> None:
@@ -36,12 +42,15 @@ class TestMatrixZeroes(TestCase):
         # Arrange
         _input1 = self.input1
         _input2 = self.input2
+        _input3 = self.input3
         # Act
         result1 = zeroes_count(_input1)
         result2 = count_zeroes(_input2)
+        result3 = zeroes(_input3)
         # Assert
         self.assertEqual(self.expected1, result1)
         self.assertEqual(self.expected2, result2)
+        self.assertEqual(self.expected3, result3)
 
 
 if __name__ == "__main__":
